f6fba564eafb7b8d406a0ab7991ad5a727f9ca5f
Sunayana Das <135920492+sunayanadas21@users.noreply.github.com>
2024-08-22 02:57:51 +0000

    created smallest/largest class and functions

--- SmallestLargest.java
+++ SmallestLargest.java
@@ -1,12 +1,28 @@
-//put your name here
+//Sunayana Das
 public class SmallestLargest{
-    public static void main(String[] args){
-
+    public static void main (String [] args){
+       int [] arr= new int[10];
+       for (int i=0; i<arr.length; i++){
+        arr[i]= (int)((Math.random()*100)+1);
+    } 
+    System.out.println(smallestValue(arr));
+    System.out.println(largestValue(arr));
     }
-    public static int smallestValue(){
-
+    public static int smallestValue(int[]a1){
+        int min= a1[0];
+        for (int i: a1){
+            if (i<min)
+            min=i;
+        }
+        return min;
     }
-    public static int largestValue(){
-        
+    public static int largestValue(int[] a2){
+        int max= a2[0];
+        for (int i: a2){
+            if (i>max)
+            max=i;
+        }
+        return max;
     }
-}
+       
+}  
